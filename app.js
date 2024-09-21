5753cfcdefd11ab1407cede195ed45980cc259f6
database-onyx <141583093+database-onyx@users.noreply.github.com>
2024-09-21 23:08:30 +0530

    Update app.js

--- app.js
+++ app.js
@@ -51,10 +51,10 @@ app.get("/", (req, res) => {
 // Route for PIN authentication
 app.post("/", async (req, res) => {
   const pin = req.body.pin;
-  if (pin === "5577") {
+  if (pin === "6454") {
     // Redirect to ssvportal1.html
     res.redirect("/ssvportal1");
-  } else if (pin === "1213") {
+  } else if (pin === "7898") {
     // Redirect to kerala.html
     res.redirect("/kerala");
   } else {
