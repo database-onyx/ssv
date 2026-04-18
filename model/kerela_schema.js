fa6f278d3ff4360a68e958a3e7a92ac62fe2b643
database-onyx <141583093+database-onyx@users.noreply.github.com>
2026-04-18 23:05:08 +0530

    Update kerela_schema.js

--- model/kerela_schema.js
+++ model/kerela_schema.js
@@ -18,5 +18,5 @@ const imageSchema = new mongoose.Schema({
 });
 
 
-const ImageModel1 = mongoose.model("portal2", imageSchema);
+const ImageModel1 = mongoose.model("portal31", imageSchema, "portal31);
 module.exports = ImageModel1;
